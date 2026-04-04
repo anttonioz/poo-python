a63e823ff462494aa728b3be00e4900152bc8250
Anttonioz <1317580+anttonioz@users.noreply.github.com>
2026-04-04 00:15:05 -0500

    feat: add Book class

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from poo-python!")
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
+first_book = Book("El padre Brown al completo", "G.K. Chesterton")
+second_book = Book("Más allá del orden", "Jordan B. Peterson")
 
-if __name__ == "__main__":
-    main()
+print(f"My book: {first_book.title} {first_book.author}")
+print(f"My book: {second_book.title} {second_book.author}")
