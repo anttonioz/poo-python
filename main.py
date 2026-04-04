de651fb47ab4dcfd6559b546b62f0df3ea3d1395
Anttonioz <1317580+anttonioz@users.noreply.github.com>
2026-04-04 00:56:45 -0500

    feat(book): add lend and give_back methods

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ class Book:
         self.author = author
         self.year = year
         self.is_available = is_available
+        self.loan_counter = 0
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.year}) - Available: {self.is_available}"
+
+    def lend(self):
+        if self.is_available:
+            self.is_available = False
+            self.loan_counter += 1
+            return f"Times borrowed: {self.loan_counter}."
+
+    def give_back(self):
+        if self.is_available == False:
+            self.is_available = True
 
 book_1 = Book(
     "El padre Brown al completo",
@@ -19,7 +33,13 @@ book_2 = Book(
     False
 )
 
+"""
 catalog = [book_1, book_2]
-
-for x in catalog:
+ for x in catalog:
     print(f"{x.title}, {x.author}. {x.year} - available: {x.is_available}")
+"""
+
+print(book_1.lend())
+print(book_1.give_back())
+print(book_1.lend())
+print(book_1.loan_counter)
